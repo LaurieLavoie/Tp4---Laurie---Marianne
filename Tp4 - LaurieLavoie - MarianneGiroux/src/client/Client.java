07a8a475df4397ab2f561471ff95e78598031f2d
Laurie <laurie13avril@hotmail.com>
2013-11-19 10:29:33 -0500

    Serveur et Client

--- Tp4 - LaurieLavoie - MarianneGiroux/src/client/Client.java
+++ Tp4 - LaurieLavoie - MarianneGiroux/src/client/Client.java
@@ -1,5 +1,160 @@
 package client;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class Client 
+{
+	
+	private Client client;
+	Socket socket;
+	
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    String message;
+//    Requester(){
+	
+	
+	public Client()
+	{
+		
+	}
+	
+	void run()
+    {
+        try{
+            //1. creating a socket to connect to the server
+        	int port = 0;
+        	port = this.enteredPort();
+            this.socket = new Socket("localhost",port);
+          
+            //2. get Input and Output streams
+            out = new ObjectOutputStream(this.socket.getOutputStream());
+            out.flush();
+            in = new ObjectInputStream(this.socket.getInputStream());
+            //3: Communicating with the server
+            do{
+                try{
+                	
+                	boolean quitter = false;
+            		message = (String)in.readObject();
+
+                	while(quitter == false)
+                	{
+                		
+                		int messageClient = clientChoice();
+                		System.out.println(messageClient);
+                		System.out.println(quitter);
+                		if (messageClient == 1)
+                		{
+
+                		}
+                		else if(messageClient == 2)
+                		{
+
+                		}
+                		else if(messageClient == 3)
+                		{
+
+                			quitter = true;
+                    		System.out.println(quitter);
+                        	System.out.println("server>" + message);
+                        	sendMessage("Hi my server");		
+//                       
+                           	message = "bye";
+                            sendMessage(message);
+
+                		}	                	
+//                    
+                	}
+                	
+                }
+                catch(ClassNotFoundException classNot){
+                    System.err.println("data received in unknown format");
+                }
+            }while(!message.equals("bye"));
+        }
+        catch(UnknownHostException unknownHost){
+            System.err.println("You are trying to connect to an unknown host!");
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            //4: Closing connection
+            try{
+                in.close();
+                out.close();
+                this.socket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+	private int clientChoice() 
+	{
+		int choice = 0;
+    	
+    	System.out.print("Entré 1 pour jouer, 2 pour voir vos scores ou 3 pour quitter: ");
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			String line = in.readLine();
+			System.out.println("Vous avez choisi :  " + choice);
+			choice = Integer.parseInt(line);
+		
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return choice;
+	}
+
+	public int enteredPort()
+	    {
+	    	int port = 0;
+	    	
+	    	System.out.println("Entrez le port désiré: ");
+			try {
+				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+				String line = in.readLine();
+				System.out.println("Client connecté sur le port :  " + line);
+
+				port =  Integer.parseInt(line);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return port;
+	    	
+	    }
+    void sendMessage(String msg)
+    {
+        try{
+
+            out.writeObject(msg);
+            out.flush();
+            System.out.println("client>" + msg);
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+    public static void main(String args[])
+    {
+        Client client = new Client();
+    	client.run();
+    }
+	
+	
 
 }
---
+++ Tp4 - LaurieLavoie - MarianneGiroux/src/serveur/Serveur.java
@@ -0,0 +1,106 @@
+package serveur;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Serveur
+{
+	 ServerSocket providerSocket;
+	    Socket connection = null;
+	    ObjectOutputStream out;
+	    ObjectInputStream in;
+	    String message;
+	    Serveur(){}
+	    void run()
+	    {
+	        try{
+
+	        	int port = 0;
+	        	port = this.enteredPort();
+	            providerSocket = new ServerSocket(port, 10);
+
+	            //2. Wait for connection
+	            System.out.println("Waiting for connection");
+	            this.connection = this.providerSocket.accept();
+	            System.out.println("Connection received from " + connection.getInetAddress().getHostName());
+	            //3. get Input and Output streams
+	            this.out = new ObjectOutputStream(connection.getOutputStream());
+	            this.out.flush();
+	            in = new ObjectInputStream(connection.getInputStream());
+	            sendMessage("Connection successful");
+	            //4. The two parts communicate via the input and output streams
+	            do{
+	            	
+	            	
+	                try{
+	                    message = (String)in.readObject();
+	                    System.out.println("client>" + message);
+	                    if (message.equals("bye"))
+	                        sendMessage("bye");
+	                }
+	                catch(ClassNotFoundException classnot){
+	                    System.err.println("Data received in unknown format");
+	                }
+	            }while(!message.equals("bye"));
+	        }
+	        catch(IOException ioException){
+	            ioException.printStackTrace();
+	        }
+	        finally{
+	            //4: Closing connection
+	            try{
+	                in.close();
+	                out.close();
+	                providerSocket.close();
+	            }
+	            catch(IOException ioException){
+	                ioException.printStackTrace();
+	            }
+	        }
+	    }
+	   
+	    
+	    public int enteredPort()
+	    {
+	    	int port = 0;
+	    	
+	    	System.out.println("Entrez le port désiré: ");
+			try {
+				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+				String line = in.readLine();
+				System.out.println("Serveur connecté sur le port :  " + line);
+
+				port =  Integer.parseInt(line);
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return port;
+	    	
+	    }
+	 void sendMessage(String msg)
+	    {
+	        try{
+	            out.writeObject(msg);
+	            out.flush();
+	            System.out.println("server>" + msg);
+	        }
+	        catch(IOException ioException){
+	            ioException.printStackTrace();
+	        }
+	    }
+	    public static void main(String args[])
+	    {
+	        Serveur server = new Serveur();
+	        while(true){
+	            server.run();
+	        }
+	    }
+}
